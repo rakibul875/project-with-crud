fa19a31c8ddc28930f5f472b6bd4974b2c6b1f86
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-12 08:50:17 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <button className='btn btn-soft btn-primary'>Click Me</button>
+       <Button variant="danger">Danger</Button>
+    </div>
+  );
+};
+
+export default HomePage;
