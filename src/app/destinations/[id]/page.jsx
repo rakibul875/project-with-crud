1d1a0802ce69db53232280d7b37c090956b28b66
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-14 00:05:52 +0600

    Add destination Details Page

---
+++ src/app/destinations/[id]/page.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import React from "react";
+import { CiEdit } from "react-icons/ci";
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa6";
+import { IoIosPin } from "react-icons/io";
+import { MdOutlineDelete } from "react-icons/md";
+
+const DestinationDetailsPage = async ({ params }) => {
+  const { id } = await params;
+  const res = await fetch(`http://localhost:8000/destination/${id}`);
+  const data = await res.json();
+  console.log(data);
+  return (
+    <div className="container mx-auto">
+      <div className="mt-5 flex justify-between lg:p-0 p-5">
+        <div className="">
+          <h1 className="text-gray-400 text-lg flex items-center gap-1">
+            <FaArrowLeft />
+            Back To Destination
+          </h1>
+        </div>
+        <div className="">
+          <button className="btn btn-outline text-gray-500">
+            <CiEdit />
+            Edit
+          </button>
+          <button className="btn btn-outline text-red-500 ml-3">
+            <MdOutlineDelete />
+            Delete
+          </button>
+        </div>
+      </div>
+
+      <div className="p-5">
+        <Image
+          src={data.imageUrl}
+          height={600}
+          width={800}
+          alt=""
+          className="w-full h-150 object-cover"
+        />
+
+        <div className="card-body p-1 mt-2">
+          <p className="text-lg text-gray-500 flex items-center gap-1">
+            <IoIosPin />
+            {data.country}
+          </p>
+          <div className="flex justify-between">
+            <h2 className="card-title text-3xl">{data.destinationName}</h2>
+            <h2 className="card-title text-lg text-gray-400">
+              <span className="text-2xl text-black">${data.price}</span>/Person
+            </h2>
+          </div>
+          <p className="text-lg text-gray-400 flex items-center gap-1">
+            <FaCalendarAlt />
+            {data.duration}
+          </p>
+          <div className="">
+            
+        <p className="text-xl">{data.description}</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DestinationDetailsPage;
--- src/components/DestinationsCard.jsx
+++ src/components/DestinationsCard.jsx
@@ -1,33 +1,50 @@
 import { Button } from "@heroui/react";
+
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaCalendarAlt } from "react-icons/fa";
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { IoIosPin } from "react-icons/io";
 
 const DestinationsCard = ({ destination }) => {
+  const { imageUrl, destinationName, country, price, duration, _id } =
+    destination;
   return (
     <div>
       <div className="card bg-base-100 shadow-lg">
         <figure className="">
           <Image
-            src={destination.imageUrl}
-            alt={destination.destinationName}
+            src={imageUrl}
+            alt={destinationName}
             width={400}
             height={250}
             className="w-full h-60 object-cover"
           />
         </figure>
         <div className="card-body p-1">
-            <p className="text-lg text-gray-500 flex items-center gap-1"><IoIosPin />{destination.country}</p>
+          <p className="text-lg text-gray-500 flex items-center gap-1">
+            <IoIosPin />
+            {country}
+          </p>
           <div className="flex justify-between">
-            <h2 className="card-title text-2xl">{destination.destinationName}</h2>
-            <h2 className="card-title text-lg text-gray-400"><span className="text-2xl text-black">${destination.price}</span>/Person</h2>
+            <h2 className="card-title text-2xl">{destinationName}</h2>
+            <h2 className="card-title text-lg text-gray-400">
+              <span className="text-2xl text-black">${price}</span>/Person
+            </h2>
           </div>
-          <p className="text-lg text-gray-400">
-            {destination.duration}
+          <p className="text-lg text-gray-400 flex items-center gap-1">
+            <FaCalendarAlt />
+            {duration}
           </p>
           <div className="card-actions">
-           <Button variant="outline" className='text-cyan-500'><FaArrowUpRightFromSquare />Book Now</Button>
+            <Link href={`/destinations/${_id}`}>
+              {" "}
+              <Button variant="outline" className="text-cyan-500">
+                <FaArrowUpRightFromSquare />
+                Book Now
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
